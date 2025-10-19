d6d3f6cc97981e1cc14058337037627039c23d1f
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-10-19 21:44:02 +0300

    Revert "Merge pull request #8 from HeshamM-088/ashraf"
    This reverts commit f8f0c7aebdd45ebd3e91aed2c33d5bb49b2e6e81, reversing
    changes made to 65187db31ece4b502bef8b30fdf8548451c8bc96.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,7 @@ module.exports = withMT({
     "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "egypt-pyramids":
-          "url('https://images.unsplash.com/photo-1525604529863-915380184a43?auto=format&fit=crop&w=1920&q=80')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 });
